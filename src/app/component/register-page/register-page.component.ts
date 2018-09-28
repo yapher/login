c1aeaf76fa1091245e3f5474b038c2bb1833a066
andres herasimovich <andres.herasi32@gmail.com>
2018-09-28 00:59:44 -0300

    registro funcionando

--- src/app/component/register-page/register-page.component.ts
+++ src/app/component/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import { Router } from '@angular/router';
 // import 'rxjs/add/operator/switchMap';
 
 @Component({
@@ -12,7 +13,7 @@ export class RegisterPageComponent implements OnInit {
   email: string;
   password: string;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, public router: Router) { }
 
   ngOnInit() {
   }
@@ -20,7 +21,7 @@ export class RegisterPageComponent implements OnInit {
   onSubmitAddUser() {
      this.authService.registerUser(this.email, this.password)
        .then( (res) => {
-         console.log('muy bien');
+         this.router.navigate(['/privado']);
          console.log(res);
        }).catch( (err) => {
          console.log(err);
